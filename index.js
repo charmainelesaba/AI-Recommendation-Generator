94318940cf8315421bc9f494acc01e9f65f6d5f8
charmainelesaba <bonelesaba@gmail.com>
2024-10-23 02:02:47 +0200

    Added javascript functionality to the website

---
+++ index.js
@@ -0,0 +1,37 @@
+function displayBookRecommendations(event) {
+    event.preventDefault();
+    
+    let bookList = document.querySelector("#book-list");
+    let bookRecommendationElement = document.querySelector("#book-recommendation");
+    
+
+    bookList.innerHTML = "";
+    bookRecommendationElement.innerHTML = "";
+
+    let typewriter = new Typewriter(bookRecommendationElement, {
+        autoStart: true,
+        delay: 50,
+        cursor: null
+    });
+
+    
+    let books = ["Game of Thrones", "Throne of Glass", "Twilight"];
+
+    
+    books.forEach((book) => {
+        typewriter
+            .typeString(book + '<br>')  
+            .pauseFor(500)              
+            .callFunction(() => {
+                
+                let listItem = document.createElement('li');
+                listItem.textContent = book;
+                bookList.appendChild(listItem);
+            })
+            .pauseFor(500)              
+            .start();
+    });
+}
+
+let formElement = document.querySelector("#form");
+formElement.addEventListener("submit", displayBookRecommendations);
